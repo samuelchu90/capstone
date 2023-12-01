472afff44323e364cc120efaf623f3713ccd80de
Samuel Chu <samuelchu90@gmail.com>
2023-12-01 15:29:57 -0800

    made progress on backpropogation, prepare_data.py now gives shuffled sequence and label data.

--- bert/bert.py
+++ bert/bert.py
@@ -2,6 +2,8 @@
 from transformers import BertTokenizer, BertModel, BertConfig
 import torch
 from torch import nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
 class MLP(nn.Module):
   '''
@@ -40,6 +42,9 @@ print(config)
 model.load_state_dict(model_arc['state_dict'], strict=True)
 print(model)
 
+def threshold(tensor):
+    return (tensor>0.5).int()
+
 class FullModel(torch.nn.Module):
     def __init__(self, bert):
         super().__init__()
@@ -49,11 +54,13 @@ class FullModel(torch.nn.Module):
     def forward(self, x):
         mask = (x>0).float()
         pooler_output = self.bert(x, attention_mask=mask).pooler_output
-        return self.linear(pooler_output)
+        linear_output = self.linear(pooler_output)
+        sigmoid_output = torch.sigmoid(linear_output)
+        return threshold(sigmoid_output)
 
 amp_aa_sequences = torch.load('data/amp_aa_sequences.pt')
 non_amp_aa_sequences = torch.load('data/non_amp_aa_sequences.pt')
-smaller_amp_aa_sequences = amp_aa_sequences[:1]
+smaller_amp_aa_sequences = amp_aa_sequences[:3]
 
 full_model = FullModel(model)
 outputs = full_model(smaller_amp_aa_sequences)
@@ -63,3 +70,45 @@ print(outputs)
 
  #batch size x #tokens x #d_model multiply this matrix by #d_model x 1
 #binary cross entropy
+
+
+#DataLoader
+class AMPDataset(Dataset):
+    def __init__(self, sequences, labels):
+        self.sequences = sequences
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        sequence = self.sequences[idx]
+        label = self.labels[idx]
+        return sequence, label
+
+#Backprop
+sequences = torch.load('data/shuffled_sequences.pt')
+labels = torch.load('data/shuffled_labels.pt')
+
+print('here')
+print(len(sequences))
+
+dataset = AMPDataset(sequences, labels)
+dataloader = DataLoader(dataset, batch_size=64)
+loss_fn = nn.BCELoss()
+optimizer = optim.Adam(full_model.parameters(), lr=.00002)
+
+num_epochs = 1
+for epoch in range(num_epochs):
+    for sequences, labels in dataloader:
+        sequences = torch.tensor(sequences, dtype=int)
+        labels = torch.tensor(labels, dtype=int)
+        optimizer.zero_grad()
+        outputs = full_model(sequences)
+        outputs = outputs.squeeze(1)
+        outputs = torch.tensor(outputs, dtype=int)
+        print(outputs.shape)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
--- bert/prepare_data.py
+++ bert/prepare_data.py
@@ -1,4 +1,6 @@
 import torch
+import random
+from sklearn.model_selection import train_test_split
 
 aa_map = {
     'A': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'K': 9,
@@ -17,10 +19,11 @@ def peptide_to_int(peptide):
 #label tells the model if a peptide is an AMP or not
 #(1 for AMP, 0 for non-AMP)
 #i think we need to add [CLS] and [SEP] still.
-def fa_to_list(fa_path, label, include_label=False):
+def fa_to_list(fa_path, label):
     max_length = 300
     ids = []
     aa_sequences = []
+    labels = []
     with open(fa_path, 'r') as file:
         for line in file:
             if line[0] == '>':
@@ -28,18 +31,67 @@ def fa_to_list(fa_path, label, include_label=False):
             else:
                 peptide = line[:-1]
                 new_rep = peptide_to_int(peptide) + [0]*(max_length-len(peptide))
-                if include_label:
-                    new_rep.append(label)
                 aa_sequences.append(new_rep)
+                labels.append(label)
     aa_sequences = torch.tensor(aa_sequences, dtype=torch.long)
-    return ids, aa_sequences
+    return ids, aa_sequences, labels
 
-amp_ids, amp_aa_sequences = fa_to_list('data/AMPs.fa', 1)
-non_amp_ids, non_amp_aa_sequences = fa_to_list('data/Non-AMPs.fa', 0)
+amp_ids, amp_aa_sequences, amp_labels = fa_to_list('data/AMPs.fa', 1)
+non_amp_ids, non_amp_aa_sequences, non_amp_labels = fa_to_list('data/Non-AMPs.fa', 0)
 print(amp_ids[:3])
 print(amp_aa_sequences[:3])
+print(amp_labels[:3])
 print(non_amp_ids[:3])
 print(non_amp_aa_sequences[:3])
+print(non_amp_labels[:3])
+
+def generate_all_data(amp_aa_sequences, non_amp_aa_sequences, torch_save = False):
+    all_data = []
+    for aa in amp_aa_sequences:
+        all_data.append((aa, 1))
+    for aa in non_amp_aa_sequences:
+        all_data.append((aa, 0))
+
+    random.shuffle(all_data)
+    sequences = []
+    labels = []
+    for tup in all_data:
+        sequences.append(tup[0])
+        labels.append(tup[1])
+
+    if torch_save:
+        torch.save(sequences, 'shuffled_sequences.pt')
+        torch.save(labels, 'shuffled_labels.pt')
+    return sequences, labels
+
+def train_test_split(sequences, labels):
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     sequences, labels, test_size=.3
+    # )
+    test_size = 0.3
+    split_idx = int(test_size*len(sequences))
+    X_test = sequences[:split_idx]
+    y_test = labels[:split_idx]
+    X_train = sequences[split_idx:]
+    y_train = labels[split_idx:]
+    return X_train, X_test, y_train, y_test
+
+shuffled_sequences, shuffled_labels = generate_all_data(amp_aa_sequences, non_amp_aa_sequences)
+X_train, X_test, y_train, y_test = train_test_split(shuffled_sequences, shuffled_labels)
+print(len(X_train))
+print(len(X_test))
+#sequences and labels contain all peptides in AMPs.fa and Non-AMPs.fa.
+#additionally they are already shuffled.
+
+
+
+
+#1085 AMP sequences and 58776 Non AMP sequences
+print(len(amp_aa_sequences))
+print(len(non_amp_aa_sequences))
+
+#[abc, 0] []
+
 
 # torch.save(amp_aa_sequences, 'amp_aa_sequences.pt')
 # torch.save(non_amp_aa_sequences, 'non_amp_aa_sequences.pt')

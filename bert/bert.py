kalen helped us fix type error. backprop is now running.

bert/bert.py
@@ -5,6 +5,8 @@ from torch import nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
+#CLS, training, test split, unbalanced dataset AMPS vs nonAMPS
+
 class MLP(nn.Module):
   '''
     Multilayer Perceptron.
@@ -43,7 +45,7 @@ model.load_state_dict(model_arc['state_dict'], strict=True)
 print(model)
 
 def threshold(tensor):
-    return (tensor>0.5).int()
+    return (tensor>0.5).float()
 
 class FullModel(torch.nn.Module):
     def __init__(self, bert):
@@ -51,9 +53,16 @@ class FullModel(torch.nn.Module):
         self.bert = bert
         self.linear = nn.Linear(768, 1)
 
+    #add threshold if we only want forward pass, but for training we don't want threshold.
+    #forward always wants to return a float.
     def forward(self, x):
         mask = (x>0).float()
         pooler_output = self.bert(x, attention_mask=mask).pooler_output
+        linear_output = self.linear(pooler_output)
+        return torch.sigmoid(linear_output)
+
+    def predict(self, x):
+        pooler_output = self.bert(x).pooler_output
         linear_output = self.linear(pooler_output)
         sigmoid_output = torch.sigmoid(linear_output)
         return threshold(sigmoid_output)
@@ -101,14 +110,12 @@ optimizer = optim.Adam(full_model.parameters(), lr=.00002)
 num_epochs = 1
 for epoch in range(num_epochs):
     for sequences, labels in dataloader:
-        sequences = torch.tensor(sequences, dtype=int)
-        labels = torch.tensor(labels, dtype=int)
+        sequences = sequences.int()
+        labels = labels.float()
         optimizer.zero_grad()
         outputs = full_model(sequences)
         outputs = outputs.squeeze(1)
-        outputs = torch.tensor(outputs, dtype=int)
         print(outputs.shape)
         loss = loss_fn(outputs, labels)
         loss.backward()
         optimizer.step()
-

bert/prepare_data.py
@@ -5,13 +5,14 @@ from sklearn.model_selection import train_test_split
 aa_map = {
     'A': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'K': 9,
     'L': 10, 'M': 11, 'N': 12, 'P': 13, 'Q': 14, 'R': 15, 'S': 16, 'T': 17,
-    'V': 18, 'W': 19, 'Y': 20
+    'V': 18, 'W': 19, 'Y': 20, '[CLS]': 21, '[SEP]': 22 
 }
 
 def peptide_to_int(peptide):
     result = []
     for aa in peptide:
         result.append(aa_map[aa])
+    result = [21] + result + [22]
     return result
 
 
@@ -30,7 +31,7 @@ def fa_to_list(fa_path, label):
                 ids.append(line[1:-1])
             else:
                 peptide = line[:-1]
-                new_rep = peptide_to_int(peptide) + [0]*(max_length-len(peptide))
+                new_rep = peptide_to_int(peptide) + [0]*(max_length-(len(peptide)+2)) #+2 for the [CLS] and [SEP] tokens
                 aa_sequences.append(new_rep)
                 labels.append(label)
     aa_sequences = torch.tensor(aa_sequences, dtype=torch.long)
@@ -76,7 +77,7 @@ def train_test_split(sequences, labels):
     y_train = labels[split_idx:]
     return X_train, X_test, y_train, y_test
 
-shuffled_sequences, shuffled_labels = generate_all_data(amp_aa_sequences, non_amp_aa_sequences)
+shuffled_sequences, shuffled_labels = generate_all_data(amp_aa_sequences, non_amp_aa_sequences, torch_save=True)
 X_train, X_test, y_train, y_test = train_test_split(shuffled_sequences, shuffled_labels)
 print(len(X_train))
 print(len(X_test))